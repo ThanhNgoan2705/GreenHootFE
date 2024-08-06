855b9c2d8032fa7c8d131170a4b4fb8f328b279d
ThanhNgoan2705 <20130335@st.hcmuaf.edu.vn>
2024-08-06 22:36:05 +0700

    edit sidebar router

--- src/router/sidebar.routes.ts
+++ src/router/sidebar.routes.ts
@@ -6,7 +6,6 @@ import UserReportDetailPage from "@/public-page/user-public-page/UserReportDetai
 const routes = [
     {path: '/UserHomePage',name:'userHome', component: UserHomePage, meta: {showHeader: false,activeLink:'Home'}},
     {path: '/UserLibraryPage',name:'userLibrary', component: UserLibraryPage, meta: {showHeader: false,activeLink:'Library'}},
-    {path: '/UserFolderPage',name:'userFolder', component: UserFolderPage, meta: {showHeader: false,activeLink:'Folder'}},
     {path: '/UserReportsPage',name:'userReports', component: UserReportsPage, meta: {showHeader: false,activeLink:'Reports'}},
     {path: '/UserReportDetailPage',name:'userReportsDetail', component: UserReportDetailPage, meta: {showHeader: false,activeLink:'ReportDetail'}},
 
